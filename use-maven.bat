4b8d04cf6f12db3eb1da05aeb4fffad63d1d9254
Prithiv Shiv <123861679+Prithiv-shiv-26@users.noreply.github.com>
2025-06-13 17:13:53 +0530

    changes

--- use-maven.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting up Maven environment...
-set PATH=%PATH%;C:\maven\apache-maven-3.9.6\bin
-echo Maven is now available in this terminal session.
-echo You can run Maven commands like: mvn clean install
-echo.
-cmd /k
